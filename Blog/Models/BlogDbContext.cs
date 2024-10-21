91dc523b3f565b968aa853b204a42682accbe2a2
JanosiMarcell <janosim@kkszki.hu>
2024-10-21 08:59:28 +0200

    Migration ready

---
+++ Blog/Models/BlogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+
+        }
+        public BlogDbContext(DbContextOptions option): base(option) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Blog; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+        public DbSet<Blogger> Bloggers { get; set; } = null!;
+    }
+}
